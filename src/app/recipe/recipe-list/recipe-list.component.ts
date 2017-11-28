2a1ba61810644caca2fd028d255c488f4ff49a53
Hermy Garcia <hermygarcia@gmail.com>
2017-11-28 09:08:04 -0400

    working in receipt

--- src/app/recipe/recipe-list/recipe-list.component.ts
+++ src/app/recipe/recipe-list/recipe-list.component.ts
@@ -9,8 +9,12 @@ import { Receipt } from '../receipt.model';
 export class RecipeListComponent implements OnInit {
 
   receipts = [
-    new Receipt('Dulce de calabaza', '', 5.0, ''),
-    new Receipt('', '', 5.0, ''),
+    new Receipt('Dulce de calabaza', 'Postres', 5.0,
+    'https://www.hola.com/imagenes/cocina/recetas/20171010100402/receta-dulce-calabaza/0-495-53/dulce-calabaza-a.jpg'),
+    new Receipt('Ensalada de verdura y pasta \'gargenelli\'', 'Ensaladas', 5.0,
+    'https://www.hola.com/imagenes/destacadas/199-21-ensalada_pasta.jpg'),
+    new Receipt('Flan de espinacas, acelgas y gambas', 'Verduras', 5.0,
+    'https://www.hola.com/imagenes/cocina/recetas/20171016100632/flan-acelgas-espinacas-gambas/0-496-658/flan-espinacas-b.jpg'),
   ];
 
   constructor() { }
